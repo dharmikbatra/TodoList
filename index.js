8bae5c8e8672363211422baf85403ab71507d031
Dharmik Batra <batradharmik@gmail.com>
2021-07-26 00:13:46 +0530

    done

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var express = require('express');
 var port = process.env.PORT || 8000;
 var app = express();
 var appRoute = require("./routes/appRoutes");
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cors = require("cors");
-mongoose.connect('mongodb+srv://dharmik:dharmik@cluster0.riqfe.mongodb.net/test',{ useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://dharmik:dharmik@cluster0.riqfe.mongodb.net/CRUD?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true },(err)=>{
+    if(!err){
+        console.log("connected database ...");
+    }else{
+        console.log("error "+ JSON.stringify(err));
+    }}
+)
 const path = require('path');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
